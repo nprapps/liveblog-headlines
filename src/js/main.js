f10dda85c3a9cf8d2db032d423f678acc447c086
thomaswilburn <twilburn@npr.org>
2020-03-16 11:32:53 -0500

    Add PJAX navigation

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,14 @@ var guest = Sidechain.registerGuest();
 
 // load the various result elements
 require("./liveblog-headlines");
+
+document.body.addEventListener("click", function(e) {
+  var target = e.target;
+  var link = target.closest("a[href]");
+  if (link) {
+    var href = link.getAttribute("href");
+    guest.sendLegacy("pjax-navigate", href);
+    console.log(`Dispatching pjax-navigate for ${href}`);
+    e.preventDefault();
+  }
+})
