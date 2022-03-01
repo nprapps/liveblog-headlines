c5a784edf797bb0eeb1944e3e402b7bf895ad466
alykat <alykat@morethanthis.net>
2022-02-28 22:56:28 -0500

    only do a PJAX link if on npr.org

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,27 @@ var guest = Sidechain.registerGuest();
 // load the various result elements
 require("./liveblog-headlines");
 
-document.body.addEventListener("click", function(e) {
-  var target = e.target;
-  var link = target.closest("a[href]");
-  if (link) {
-    var href = link.getAttribute("href");
-    guest.sendLegacy("pjax-navigate", href);
-    console.log(`Dispatching pjax-navigate for ${href}`);
-    e.preventDefault();
+var checkIfNPR = function() {
+  var params = new URLSearchParams(window.location.search);
+  var parentURL = new URL(decodeURI(params.get("parentUrl")));
+
+  if (parentURL.hostname.includes("npr.org")) {
+    return true;
+  } else {
+    return false;
   }
-})
+}
+
+var isNPR = checkIfNPR();
+if (isNPR) {
+  document.body.addEventListener("click", function(e) {
+    var target = e.target;
+    var link = target.closest("a[href]");
+    if (link) {
+      var href = link.getAttribute("href");
+      guest.sendLegacy("pjax-navigate", href);
+      console.log(`Dispatching pjax-navigate for ${href}`);
+      e.preventDefault();
+    }
+  })
+}
